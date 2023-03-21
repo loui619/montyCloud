0590fa9ce50d6955e98ec117988f7dbe45ce40ac
Nikhil Louis <nikhilouis619@gmail.com>
2023-03-21 17:04:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import { useEffect,useState } from 'react';
+import './App.css';
+const locations = ["kerala","karnataka","Maharashtra","Tamilnadu","Goa","Gujarat"]
+function App() {
+
+  const [filteredList, setFilteredList] = useState(locations);
+  const [selectedItem,setSelectedItem] = useState();
+    useEffect(()=>{
+      state();
+    },[])
+  function selectOption(e){
+    setSelectedItem(e.target.innerHTML);
+  }
+  const filterBySearch = (event) => {
+    const query = event.target.value;
+    let updatedLocations = [...locations];
+    updatedLocations = updatedLocations.filter((item) => {
+      return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    setFilteredList(updatedLocations);
+  
+  }
+  const state = ()=>{
+    return filteredList.map((item, index) => {
+      return <li key={index} onClick={selectOption}>{item}</li>
+    })
+    }
+  return (
+    <div className="App">
+      <input type="text" onChange={filterBySearch} value={selectedItem}/>
+    <div id="item-list">
+    <ul>
+      {state()}
+    </ul>
+  </div>
+  </div>
+  );
+}
+
+export default App;
